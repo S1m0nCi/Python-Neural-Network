1f8d524b121423074afffb615209c962263ad5ce
Simon Ciavarro <115393581+S1m0nCi@users.noreply.github.com>
2024-06-22 22:47:51 +0100

    implemented feed_forward function

--- python-neural-network/classes/Layer.py
+++ python-neural-network/classes/Layer.py
@@ -4,7 +4,17 @@ from Node import Node
 class Layer:
   def __init__(self, nodes):
     self.nodes = nodes
+  
+  # unsure about below
+  def compute_layer(self, inputs: list[list[float]]):
+    return [self.nodes[i].compute(inputs[i]) for i in range(len(self.nodes))]
+  # above function still not in use
 
-  def compute_layer(inputs: list[list[float]], layer: list[Node]):
-    return [layer[i].compute(inputs[i]) for i in range(len(layer))]
+  def pass_layer(self, result, link):
+    # return some inputs (initial)
+    # just replace the indexes in link with the actual values in result
+    return [[result[i] for i in range(len(link[j]))] for j in range(len(link))]
+
+  def length(self):
+    return len(self.nodes)
   
--- python-neural-network/classes/Network.py
+++ python-neural-network/classes/Network.py
@@ -8,9 +8,21 @@ class Network:
   # how do we work out how the nodes are connected
   # maybe we should leave this to personalisation ie to the user's choice: give the user fine-tune control
   # we can also have different options for connections, like 'dense', '2', etc
-  def __init__(self, layers: list[int], connection: str = "two", activation: str = "sigmoid", loss: str = "mean square error"):
-    self.layers = layers
+  # layers includes input layer and output layers
+  def __init__(self,
+               layers: list[int]= [2,2,1],
+               connection: str = "two", 
+               activation: str = "sigmoid", 
+               loss: str = "mean square error", 
+               links=[[[0,1], [0,1]], [[0], [0]]]
+               ):
+    # the 2 below is specific to the 'two' option
     self.nodes = [[Node(np.ones(2).tolist(), 0) for i in range(len(layers[j]))] for j in range(len(layers))]
+    self.layers = [Layer(self.nodes[i]) for i in range(len(self.nodes))]
+    # the links between layers should be below
+    # for now, have simple halving links
+    # the output of each layer will go to a layer with half as many nodes
+    self.links = links
     self.connection = connection
     self.activation = activation
     self.loss = loss
@@ -18,9 +30,17 @@ class Network:
   # for now, we will define all functions using the default settings, just for now
   # then we will make a 'train' function bringing everything together
   # start by computing for the input layer
-  def compute_layer(inputs: list[list[float]], layer: list[Node]):
-    return [layer[i].compute(inputs[i]) for i in range(len(layer))]
 
-  def feed_forward(self):
-    initial_inputs = []
-    # inputs = compute_layer(initial_inputs, )
+  # can we use recursion?
+  # repeatedly compute layer to get the final output
+  def feed_forward(self, initial):
+    # create the first input moulding to the input layer
+    current = self.layers[0].pass_layer(initial, self.links[0])
+    for i in range(1, len(self.layers)-1):
+      layer_result = self.layers[i].compute_layer(current)
+      current = self.layers[i].pass_layer(self.links[i])
+    layer_result = self.layers[len(self.layers)-1].compute_layer(current)
+    return layer_result
+    # returns the result for the last node(s)
+
+  
